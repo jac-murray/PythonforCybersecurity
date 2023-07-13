906baaafc9092486f9fb5226b799be5c88e732af
jac-murray <murray.jacqueline@student.greenriver.edu>
2023-07-12 21:04:38 -0700

    Greeting Experiments

--- start/CH02/HelloWorld3.py
+++ start/CH02/HelloWorld3.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
 # A more complex "Hello World" script in python with Inputs
 # Created 
+
+#Ask user for name
+user_name = input("What is your name? ")
+
+#Print hello and name
+print("Hello {0}".format(user_name))
+print("Hello " + user_name)
+message = "Hello " + user_name
+print(message)
