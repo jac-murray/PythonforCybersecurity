5f966e00d0923421ee43039189e851563f621e4d
jac-murray <murray.jacqueline@student.greenriver.edu>
2023-07-12 20:52:24 -0700

    Started HW 2

--- start/CH02/HelloWorld2.py
+++ start/CH02/HelloWorld2.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 # A simple "Hello World" script in python with Inputs
-# Created 
+# Created by Jac Murray 7/12
 
 # Suggestion, build out 1 line at a time
 # Once multiple print statemetns exist, put a breakpoint at first print line
 # Then walk through as an example of "debugging"
+
+#Ask user for name
+user_name = input("What is your name? ")
+
+#Print hello and name
+print("Hello {0}".format(user_name))
