7005e99eb308ce17c0251f4e0bde731d1c471c9e
jac-murray <murray.jacqueline@student.greenriver.edu>
2023-07-13 20:05:12 -0700

    HW4 Age Experimenting

---
+++ start/CH02/HelloWorld4.py
@@ -0,0 +1,9 @@
+
+#Ask user for name
+user_name = input("What is your name? ")
+
+#Print hello and name
+print(f"Hello {user_name}, I hope you're having a wonderful day.")
+
+#Ask for age
+age = int(input("How old are you? "))
