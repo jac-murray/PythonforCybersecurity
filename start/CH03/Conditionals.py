359b91cda2545766ca2724e2b626a0600fb7aa28
jac-murray <murray.jacqueline@student.greenriver.edu>
2023-07-16 13:33:54 -0700

    Looped Response

--- start/CH03/Conditionals.py
+++ start/CH03/Conditionals.py
@@ -4,6 +4,10 @@
 
 good_day = input("Is today a good day? (Y/N)")
 if good_day == "Y" or good_day == "y":
-  print("Yes, it is!")
+
+  #added loop 7/16
+  
+  for x in range (10):
+    print("Yes, it is!")
 if good_day == "N" or good_day == "n":
   print("I'm sorry.")
