3ee9a75e9a37a4cc85e5b2a6dae538114a7c6045
jac-murray <murray.jacqueline@student.greenriver.edu>
2023-07-15 18:12:57 -0700

    Good Day Conditional

--- start/CH03/Conditionals.py
+++ start/CH03/Conditionals.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
 # example workign with conditionals
-#By 
+#By Jac 7/15
+
+good_day = input("Is today a good day? (Y/N)")
+if good_day == "Y" or good_day == "y":
+  print("Yes, it is!")
+if good_day == "N" or good_day == "n":
+  print("I'm sorry.")
