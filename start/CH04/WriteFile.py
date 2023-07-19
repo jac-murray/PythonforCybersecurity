559b66877445a04fc2665584e695b668642e988d
jac-murray <murray.jacqueline@student.greenriver.edu>
2023-07-18 19:51:21 -0700

    Write Test File

--- start/CH04/WriteFile.py
+++ start/CH04/WriteFile.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python3
 # Sample script that writes to a file
-# By 
+# By Jac 7/18
+
+import os
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+#Open file for writing
+f = open(dir_path + "/testfile.txt", "w")
+
+#Write to the file
+print("hello world")
+f.write("Hello World\n")
+f.write("Hello World\n")
+f.write("Hello World\n")
+
+#Close file
+f.close()
