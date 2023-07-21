76b6c859f9cbc2df35214c525eee174bf6d1fe3a
jac-murray <murray.jacqueline@student.greenriver.edu>
2023-07-20 19:27:18 -0700

    2 - Read File

--- start/CH04/ReadFile.py
+++ start/CH04/ReadFile.py
@@ -1,3 +1,21 @@
 #!/usr/bin/env python3
 # Sample script that reads from a file
-# By 
+# By Jac 7/20
+
+
+import os
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+# Open file for reading
+
+f = open(dir_path + "/hackme.txt", "r")
+print("Here is someone to hack - information")
+
+# Read file and print
+
+info = f.read()
+print(info)
+print(f)
+
+# Close file
+f.close()
