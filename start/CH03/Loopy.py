4f8c31782128d17038969e835a2f2fd2d418551a
jac-murray <murray.jacqueline@student.greenriver.edu>
2023-07-21 17:28:54 -0700

    Proper Lop File

--- start/CH03/Loopy.py
+++ start/CH03/Loopy.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python3
 # example workign with Loops
-#By 
+#By Jac 716
+
+good_day = input("Is today a good day? (Y/N)")
+if good_day in {"Y", "y"}:
+  
+  for x in range (10):
+    print("Yes, it is!")
+if good_day in {"N", "n"}:
+  print("I'm sorry.")
