93b8371219238f1f7d5cd22512cd3cd135dc1c57
jac-murray <murray.jacqueline@student.greenriver.edu>
2023-07-21 17:41:59 -0700

    Send_Message Function

--- start/CH03/Funcy.py
+++ start/CH03/Funcy.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python3
 # example workign with Functions
-#By 
+#By Jac 7/21
+
+#function
+def send_message():
+    good_day = input("Is today a good day? (Y/N) ")
+
+    if good_day in {"Y", "y"}:
+        for x in range(10):
+            print("Yeah it is!")
+
+send_message()
