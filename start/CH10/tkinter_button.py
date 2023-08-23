3ebf88236b1f540ed0804d2d95a091fc2f306c22
jac-murray <murray.jacqueline@student.greenriver.edu>
2023-08-22 18:31:29 -0700

    Added Button

--- start/CH10/tkinter_button.py
+++ start/CH10/tkinter_button.py
@@ -1,13 +1,27 @@
 # Seconder tkinter script
 # Add a button and command
-# Create 
+# Created by Jac 8/22/2023 
 
 # Import tkinter
+from tkinter import *
+from tkinter import messagebox
 
 # Functions
+def button_clicked():
+    Label(window, text = "button was clicked").pack()
+    messagebox.showerror("Error", "Don't click that")
 
 # Create the GUI main window
+window = Tk()
+window.title("Hello Python")
+window.geometry('300x200')
 
 # Add widgets
+lbl = Label(window, text="Hello, world")
+
+    #Button Widget
+button = Button(window, text = "click Here", command = button_clicked)
+button.pack()
 
 # Enter the main event loop
+window.mainloop()
