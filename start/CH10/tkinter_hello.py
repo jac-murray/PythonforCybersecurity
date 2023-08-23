3cffebee21394e1e49686256bc8c5b09a653021f
jac-murray <murray.jacqueline@student.greenriver.edu>
2023-08-22 18:16:30 -0700

    Basic GUI

--- start/CH10/tkinter_hello.py
+++ start/CH10/tkinter_hello.py
@@ -1,10 +1,17 @@
 # First tkinter script
-# Create by 
+# Create by Jac 8/22/2023
 
 # Import tkinter
+from tkinter import *
 
 # Create the GUI main window
+window = Tk()
+window.title("Hello Python")
+window.geometry('300x200')
 
 # Add widgets
+lbl = Label(window, text="Hello, world")
+lbl.grid(column=0, row=0)
 
 # Enter the main event loop
+window.mainloop()
